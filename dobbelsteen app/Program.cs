1c06bb00cdaa9d6023b8ccbffc93001fcf413b3f
Rajeev Willems <2404378willems@zuyd.nl>
2024-12-17 10:42:53 +0100

    dobbelsteen start

---
+++ dobbelsteen app/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dobbelsteen_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
